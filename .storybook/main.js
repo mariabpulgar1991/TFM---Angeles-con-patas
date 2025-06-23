ee5df5fa02cc60d19c8afc59a7085a2a4f2765ec
mariabpulgar <mariabpulgar@gmail.com>
2025-06-23 19:24:18 -0400

    commit inicial

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/preset-create-react-app"
+  ],
+  "framework": {
+    "name": "@storybook/react-webpack5",
+    "options": {}
+  },
+  "staticDirs": [
+    "../public"
+  ]
+};
+export default config;
