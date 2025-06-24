7e85d572c5bf0de4612bc1f2dfeea85c827000f5
Julian Figueroa <julianfigxix@gmail.com>
2025-06-23 20:00:31 -0500

    colors

---
+++ src/tokens/colors.js
@@ -0,0 +1,88 @@
+const colors = {
+  
+  //Ciam
+  ciamLight: '#f2f6f9',
+  ciamLightHover: '#e8f0f6',
+  ciamLightActive: '#cde4f5',
+  ciamNormal: '#3c8dbc',
+  ciamNormalHover: '#357ca5',
+  ciamNormalActive: '#2e6e93',
+  ciamDark: '#275f7a',
+  ciamDarkHover: '#1f4f66',
+  ciamDarkActive: '#183f52',
+  ciamDarker: '#0f2f3d',
+
+  //Blue
+  blueLight: '#e7e7f9',
+  blueLightHover: '#dbdbf6',
+  blueLightActive: '#cacaf0',
+  blueNormal: '#252d91',
+  blueNormalHover: '#1f2884',
+  blueNormalActive: '#1b2479',
+  blueDark: '#161d61',
+  blueDarkHover: '#11174f',
+  blueDarkActive: '#0c123d',
+  blueDarker: '#080d30',
+
+  //Magenta
+  magentaLight: '#ffe7f2',
+  magentaLightHover: '#ffdbe9',
+  magentaLightActive: '#ffcfe0',
+  magentaNormal: '#ff007b',
+  magentaNormalHover: '#e60072',
+  magentaNormalActive: '#cc0066',
+  magentaDark: '#99004d',
+  magentaDarkHover: '#7a003e',
+  magentaDarkActive: '#5c0030',
+  magentaDarker: '#400021',
+
+  //Yellow
+  yellowLight: '#fffce5',
+  yellowLightHover: '#fff8cc',
+  yellowLightActive: '#fff4b3',
+  yellowNormal: '#d9b700',
+  yellowNormalHover: '#c4a500',
+  yellowNormalActive: '#b19600',
+  yellowDark: '#8f7900',
+  yellowDarkHover: '#7a6700',
+  yellowDarkActive: '#665600',
+  yellowDarker: '#4d4100',
+
+  //Error
+  errorLight: '#ffe7e7',
+  errorLightHover: '#ffdbdb',
+  errorLightActive: '#ffcece',
+  errorNormal: '#ff0000',
+  errorNormalHover: '#e60000',
+  errorNormalActive: '#cc0000',
+  errorDark: '#990000',
+  errorDarkHover: '#7a0000',
+  errorDarkActive: '#5c0000',
+  errorDarker: '#400000',
+
+  //Gray
+  grayLight: '#f5f5f5',
+  grayLightHover: '#eeeeee',
+  grayLightActive: '#e0e0e0',
+  grayNormal: '#cccccc',
+  grayNormalHover: '#bbbbbb',
+  grayNormalActive: '#aaaaaa',
+  grayDark: '#888888',
+  grayDarkHover: '#666666',
+  grayDarkActive: '#444444',
+  grayDarker: '#2a2a2a',
+
+  //Sucess
+  successLight: '#e6f9ee',
+  successLightHover: '#d0f4e2',
+  successLightActive: '#b8eed5',
+  successNormal: '#28c76f',
+  successNormalHover: '#21b862',
+  successNormalActive: '#1ba954',
+  successDark: '#198f44',
+  successDarkHover: '#157a39',
+  successDarkActive: '#11652e',
+  successDarker: '#0c4f22',
+};
+
+export default colors;
