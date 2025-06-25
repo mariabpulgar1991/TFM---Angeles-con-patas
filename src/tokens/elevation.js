f6e28550d2d3eb0bc2467fd239e41ec3d9e16a16
Julian Figueroa <julianfigxix@gmail.com>
2025-06-24 22:20:17 -0500

    todos los tokens

---
+++ src/tokens/elevation.js
@@ -0,0 +1,12 @@
+const elevation = {
+  none: 'none',
+  inset: '0px 1px 3px 0px rgba(47, 47, 47, 0.05)',
+  xsmall: '0px 2px 6px 0px rgba(47, 47, 47, 0.10)',
+  small: '0px 4px 8px 0px rgba(47, 47, 47, 0.15)',
+  medium: '0px 6px 10px 0px rgba(47, 47, 47, 0.20)',
+  large: '0px 8px 12px 0px rgba(47, 47, 47, 0.25)',
+  xlarge: '0px 12px 17px 0px rgba(47, 47, 47, 0.30)',
+  xxlarge: '0px 16px 24px 0px rgba(47, 47, 47, 0.40)',
+};
+
+export default elevation;
---
+++ src/tokens/spacing.js
@@ -0,0 +1,22 @@
+const spacing = {
+  space0: 0,
+  space025: 1,
+  space050: 2,
+  space100: 4,
+  space150: 6,
+  space200: 8,
+  space300: 12,
+  space400: 16,
+  space500: 20,
+  space600: 24,
+  space800: 32,
+  space1000: 40,
+  space1200: 48,
+  space1600: 64,
+  space2000: 80,
+  space2400: 96,
+  space2800: 112,
+  space3200: 128,
+};
+
+export default spacing;
